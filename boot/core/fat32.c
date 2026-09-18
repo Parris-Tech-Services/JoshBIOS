96eec63994a8db66d7509c24f9423dc4d1468bc3
Joshua Luke Parris <130725599+joshualparris@users.noreply.github.com>
2026-09-18 22:22:04 +1000

    boot: make storage core freestanding-safe

--- boot/core/fat32.c
+++ boot/core/fat32.c
@@ -2,7 +2,14 @@
 
 #include <stddef.h>
 #include <stdint.h>
+
+#if __STDC_HOSTED__
 #include <string.h>
+#else
+void *memcpy(void *restrict destination, const void *restrict source, size_t length);
+void *memset(void *destination, int value, size_t length);
+int memcmp(const void *left, const void *right, size_t length);
+#endif
 
 #define FAT32_MIN_CLUSTERS 65525u
 #define FAT32_EOC_MIN      0x0FFFFFF8u
